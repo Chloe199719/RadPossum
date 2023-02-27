25ea7a6495fc5694a3e546c6d26c1cc539b2e480
Chloe <chloevision97@gmail.com>
2023-02-27 17:07:09 +0100

    basic front End Demo

--- app/dashboard/codes/Form.tsx
+++ app/dashboard/codes/Form.tsx
@@ -1,14 +1,20 @@
 import pb from "@/lib/pocketbase";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { json } from "stream/consumers";
 
-pb;
+interface formData {
+  code: string;
+  discordID: string;
+  message: string;
+}
+
 type Props = {
   date: Date;
   selHour: string;
 };
 function Form({ date, selHour }: Props) {
-  const Submit = async function (e: React.FormEvent) {
-    e.preventDefault();
+  const { register, handleSubmit, reset } = useForm<formData>();
+  const Submit: SubmitHandler<formData> = async function (data) {
     try {
       const test = await fetch("/api/code_booking", {
         method: `POST`,
@@ -17,9 +23,9 @@ function Form({ date, selHour }: Props) {
           clientEmail: pb.authStore.model?.email,
           bookedHour: selHour,
           time: date,
-          discordID: `Chloe🌸#9917`, // test data
-          message: `TEST`, //test data
-          code: `y38aywfYdu9Y0nDRXpx5FdcsQU-nC7`, // test date
+          discordID: data.discordID, // test data
+          message: data.message, //test data
+          code: data.code, // test date
         }),
       });
       const res = await test.json();
@@ -29,12 +35,30 @@ function Form({ date, selHour }: Props) {
     }
   };
   return (
-    <form onSubmit={Submit} className="flex items-center justify-center gap-2">
-      <label htmlFor="code">Your Code:</label>
-
-      <input type="text" required />
-
-      <button>Book</button>
+    <form
+      onSubmit={handleSubmit(Submit)}
+      className="flex flex-col items-center justify-center gap-2"
+    >
+      <div className="flex gap-2">
+        <div>
+          <label htmlFor="code">Your Code:</label>
+          <input id="code" type="text" required {...register("code")} />
+        </div>{" "}
+        <div>
+          <label htmlFor="discordID">DiscordID:</label>
+          <input
+            id="discordID"
+            type="text"
+            required
+            {...register(`discordID`)}
+          />
+        </div>{" "}
+        <div>
+          <label htmlFor="message">Message:</label>
+          <input id="message" type="text" {...register(`message`)} />
+        </div>
+      </div>
+      <button className=" bg-blue-600 px-6 py-2 rounded-sm">Book</button>
     </form>
   );
 }
--- lib/codesProcesss/setCodeUsed.ts
+++ lib/codesProcesss/setCodeUsed.ts
@@ -1,5 +1,7 @@
 import pb from "../pocketbase";
 
 export default async function setCodeUsed(id: string) {
-  pb.collection(`codes`).update(id, { used: true }, { APIKEY: "412312312" });
+  await pb
+    .collection(`codes`)
+    .update(id, { used: true }, { APIKEY: "412312312" });
 }
