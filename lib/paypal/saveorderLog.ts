ce3618ee51a5a2af5c796feb54c1a220b784b7d0
Chloe <chloevision97@gmail.com>
2023-03-05 23:04:19 +0100

    Order Log

--- lib/paypal/saveorderLog.ts
+++ lib/paypal/saveorderLog.ts
@@ -1,4 +1,5 @@
 import pb from "../pocketbase";
+import prismaClient from "../prisma/prismaClient";
 
 interface data {
   invoice_id: string;
@@ -14,15 +15,14 @@ export default async function saveOrderLogPaypal({
   item_bought,
 }: data) {
   try {
-    const log = await pb.collection(`paypal_order_logs`).create(
-      {
-        invoice_id,
-        value,
-        booking,
-        item_bought,
+    const log = await prismaClient.paypal_order_logs.create({
+      data: {
+        invoice_id: invoice_id,
+        value: value,
+        bookingId: booking as string,
+        item_bought: item_bought,
       },
-      { API_KEY: process.env.API_KEY }
-    );
+    });
     return log;
   } catch (error) {}
 }
---
+++ prisma/migrations/20230305220048_micro_patch2/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "paypal_order_logs" ALTER COLUMN "value" SET DATA TYPE TEXT;
