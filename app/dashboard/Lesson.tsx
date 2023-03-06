93bb01f76d3280a36bc7477e856ee4cf5286096c
Chloe <chloevision97@gmail.com>
2023-03-06 05:45:13 +0100

    Bug fixing preventing Compile

--- app/dashboard/Lesson.tsx
+++ app/dashboard/Lesson.tsx
@@ -1,7 +1,7 @@
+"use client";
 import React, { useState } from "react";
 import { ArrowDownIcon, ArrowUpIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
-import { lessons } from "@prisma/client";
 
 type Props = {
   lessonData: {
